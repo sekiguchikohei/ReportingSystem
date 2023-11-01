029cdcb4b8c70ca764eebe4f682126db522c33cf
Rika Saito <wind10cloud@gmail.com>
2023-11-01 16:02:16 +0900

    以下の初期設定を実施 ・ApplicationUser作成とIdentityUserの継承 ・初期ログイン時のRole作成とAdminユーザー自動作成機能 ・CSSファイルをSCSSファイルに変更 ・Identityのlogin, logout, registerファイルのスキャッフォールディング

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace 業務報告システム.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ProjectName { get; set; }
+    }
+}
