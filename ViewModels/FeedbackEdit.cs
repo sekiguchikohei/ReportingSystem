de4585d5e2a68cdcebdea7e0ec5f370471c20534
sekiguchikohei <k.sekiguchi305@gmail.com>
2023-11-02 15:24:03 +0900

    ViewModelの作成

---
+++ ViewModels/FeedbackEdit.cs
@@ -0,0 +1,14 @@
+﻿using 業務報告システム.Models;
+
+namespace 業務報告システム.ViewModels
+{
+    public class FeedbackEdit
+    {
+        // 単数==========================================
+        public Feedback Feedback { get; set; }
+
+        public Report Report { get; set; }
+
+        public ApplicationUser User { get; set; }
+    }
+}
---
+++ ViewModels/TodoCRUD.cs
@@ -0,0 +1,12 @@
+﻿using 業務報告システム.Models;
+
+namespace 業務報告システム.ViewModels
+{
+    public class TodoCRUD
+    {
+        // 単数==========================================
+        public Todo Todo { get; set; }
+
+        public ApplicationUser User { get; set; }
+    }
+}
