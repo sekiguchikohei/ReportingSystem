190536a85bc2e7ed9f33ae703c667c04b671cd39
sekiguchikohei <k.sekiguchi305@gmail.com>
2023-11-02 11:20:59 +0900

    Attendance,Feedback,Report,Todoの作成

--- Models/Feedback.cs
+++ Models/Feedback.cs
@@ -3,21 +3,29 @@ using System.ComponentModel.DataAnnotations;
 
 namespace 業務報告システム.Models
 {
+    //mgrのrepoatへのFB
     public class Feedback
     {
+        //PK
         public int FeedbackId { get; set; }
 
+        //既読
         public bool Confirm { get; set; } = false;
 
+        //1～3の3段階評価
         [Range(1,3)]
         public int Rating { get; set; }
 
+        //レポートに対するコメント
         public string? Comment { get; set; }
 
+        //mgrの名前
         public string Name { get; set; }
 
+        //FK
         public int ReportId { get; set; }
 
+        //NP
         public Report Report { get; set; }
     }
 }
---
+++ Models/Report.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace 業務報告システム.Models
+{
+    //作業内容
+    public class Report
+    {
+        //PK
+        public int ReportId { get; set; }
+
+        //提出日時
+        public DateTime Date { get; set; }
+
+        //今日のコメント
+        public string Comment { get; set; }
+
+        //明日の予定
+        public string TommorowComment { get; set; }
+
+        //User_FK
+        [ForeignKey("UesrId")]
+        public string UserId { get; set; }
+
+        //User_NP
+        public ApplicationUser ApplicationUser { get; set; }
+
+        //Todo_FK
+        public int TodoId { get; set; }
+
+        //Todo_NP
+        public Todo Todo { get; set; }
+    }
+}
