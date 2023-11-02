41361e6318302cb8cf74807b2d0ec1db32d51879
sekiguchikohei <k.sekiguchi305@gmail.com>
2023-11-02 09:53:25 +0900

    feedbackmedel作成

---
+++ Models/Feedback.cs
@@ -0,0 +1,23 @@
+﻿using NuGet.Protocol;
+using System.ComponentModel.DataAnnotations;
+
+namespace 業務報告システム.Models
+{
+    public class Feedback
+    {
+        public int FeedbackId { get; set; }
+
+        public bool Confirm { get; set; } = false;
+
+        [Range(1,3)]
+        public int Rating { get; set; }
+
+        public string? Comment { get; set; }
+
+        public string Name { get; set; }
+
+        public int ReportId { get; set; }
+
+        public Report Report { get; set; }
+    }
+}
