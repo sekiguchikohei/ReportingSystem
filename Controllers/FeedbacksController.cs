8f786e815a68f41eea6dca0ec4a37b6171de9c24
hiyori.x5@gmail.com <hiyori.x5@gmail.com>
2023-11-07 14:22:37 +0900

    feedback scaffolding

---
+++ Controllers/FeedbacksController.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using 業務報告システム.Data;
+using 業務報告システム.Models;
+
+namespace 業務報告システム.Controllers
+{
+    [Authorize(Roles = "Manager")]
+    public class FeedbacksController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public FeedbacksController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Feedbacks
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.feedback.Include(f => f.Report);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Feedbacks/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.feedback == null)
+            {
+                return NotFound();
+            }
+
+            var feedback = await _context.feedback
+                .Include(f => f.Report)
+                .FirstOrDefaultAsync(m => m.FeedbackId == id);
+            if (feedback == null)
+            {
+                return NotFound();
+            }
+
+            return View(feedback);
+        }
+
+        // GET: Feedbacks/Create
+        public IActionResult Create()
+        {
+            ViewData["ReportId"] = new SelectList(_context.report, "ReportId", "ReportId");
+            return View();
+        }
+
+        // POST: Feedbacks/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("FeedbackId,Confirm,Rating,Comment,Name,ReportId")] Feedback feedback)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(feedback);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ReportId"] = new SelectList(_context.report, "ReportId", "ReportId", feedback.ReportId);
+            return View(feedback);
+        }
+
+        // GET: Feedbacks/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.feedback == null)
+            {
+                return NotFound();
+            }
+
+            var feedback = await _context.feedback.FindAsync(id);
+            if (feedback == null)
+            {
+                return NotFound();
+            }
+            ViewData["ReportId"] = new SelectList(_context.report, "ReportId", "ReportId", feedback.ReportId);
+            return View(feedback);
+        }
+
+        // POST: Feedbacks/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("FeedbackId,Confirm,Rating,Comment,Name,ReportId")] Feedback feedback)
+        {
+            if (id != feedback.FeedbackId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(feedback);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FeedbackExists(feedback.FeedbackId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ReportId"] = new SelectList(_context.report, "ReportId", "ReportId", feedback.ReportId);
+            return View(feedback);
+        }
+
+        // GET: Feedbacks/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.feedback == null)
+            {
+                return NotFound();
+            }
+
+            var feedback = await _context.feedback
+                .Include(f => f.Report)
+                .FirstOrDefaultAsync(m => m.FeedbackId == id);
+            if (feedback == null)
+            {
+                return NotFound();
+            }
+
+            return View(feedback);
+        }
+
+        // POST: Feedbacks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.feedback == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.feedback'  is null.");
+            }
+            var feedback = await _context.feedback.FindAsync(id);
+            if (feedback != null)
+            {
+                _context.feedback.Remove(feedback);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FeedbackExists(int id)
+        {
+          return (_context.feedback?.Any(e => e.FeedbackId == id)).GetValueOrDefault();
+        }
+    }
+}
